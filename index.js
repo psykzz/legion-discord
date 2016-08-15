70700ce480f3e87693015f43ee4d07d54aaef76e
PsyKzz <matt.daemon660@gmail.com>
2016-08-16 00:04:21 +0100

    Add 8ball by request

--- index.js
+++ index.js
@@ -71,6 +71,17 @@ client.on("message", function(message) {
         message.reply(`it's ${moment().format("ddd Do "/*MMM*/+"@ HH:mm")}`)
     }
 
+    if(message.content.indexOf("!8ball") === 0) {
+        var answers = [
+        "It is certain", "It is decidedly so", "Without a doubt", "Very doubtful",
+        "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", 
+        "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", 
+        "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+        "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good"],
+        answer = answers[Math.floor(Math.random()*answers.length)];
+        message.reply(answer)
+    }
+
     // if(message.content.indexOf("!doit") === 0) {
     //     message.reply(`alright, shitting in @Alljump's bed`)
     // }

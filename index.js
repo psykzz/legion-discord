bd7d616891a5368a10749f127e206aa1fab6456e
PsyKzz <matt.daemon660@gmail.com>
2016-08-15 22:45:56 +0100

    Add raid manager

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 var Discord = require("discord.js"),
     CronJob = require('cron').CronJob,
     time = require('time'),
-    TIMEZONE = "Europe/France";
+    moment = require('moment'),
+    Sugar = require('sugar'),
+    TIMEZONE = "Europe/London";
+
+// default momenttimezone
+moment.tz.setDefault(TIMEZONE);
+
 
 var client = new Discord.Client(),
     invasionFrequencyHours = 4,
     _cronFrequency = "0 */4 * * *";
 
+
+var raids = new Array()
+
+function betterSplit(message, token) {
+    token = token || " "
+    var i = message.indexOf(token);
+    return [message.slice(0, i), message.slice(i+token.length)];
+}
 
 function sendToAll(message) {
     client.servers.forEach(function(server) {
@@ -43,10 +57,85 @@ client.on('message', function(message) {
 
 client.on("message", function(message) {
     if(message.content.indexOf("!help") === 0) {
-        return client.reply(message, "Legion Timer bot - Created by PsyKzz\n" +
+        return client.reply(message, "Legion bot - Created by PsyKzz\n" +
             "Commands:\n" + 
             "!invasion - How long until the next invasion\n" + 
+            "!raids - Show upcoming raids with >> `[id] <desc> on <date>`\n" + 
+            "!raid <desc> on <date> - Create a new raid with <desc> on <date>\n" + 
+            "!killraid <id> - Delete  raid by its <id> (use !raids for help)\n" + 
             "!help - This help text");
+    }
+
+    if(message.content.indexOf("!raid ") === 0) {
+        var msg = betterSplit(message.content)[1]
+
+        var description = betterSplit(msg, " on ")[0]
+        var timing = betterSplit(msg, " on ")[1]
+
+
+        // Going back 10 times see if we can get a date
+        var count = 0,
+            eventDate,
+            currentWord = '';
+        timing.split(" ").reverse().every(function(word) {
+            currentWord = `${word} ${currentWord}`.trim() 
+            _eventDate = Sugar.Date.create(currentWord)
+            if (_eventDate) {
+                eventDate = _eventDate;
+            }
+            if (eventDate && !_eventDate) {
+                return false;
+            }
+            return (count++ < 10); // stop at larger count
+        })
+
+        message.reply(`Creating new raid: ${description} on ${moment(eventDate).format("ddd Do "/*MMM*/+" @ HH:mm")}`);
+
+        raids[Object.keys(raids).length] = {
+            time: eventDate,
+            desc: description
+        }
+    }
+
+    if(message.content.indexOf("!raids") === 0) {
+        // Cleanse the raids of old shit
+        var now = moment();
+        raids.forEach(function(raid, idx) {
+            if (raid['time'] < now) {
+                // Old raid
+                console.log(`Pruning old raid: #${idx} ~ ${raid['desc']} on ${moment(raid['time']).format("ddd Do "/*MMM*/+" @ HH:mm")}`)
+                delete raids[idx]
+                return;
+            }
+        })
+
+        // Check for any raids left
+        if (!Object.keys(raids).length) {
+            return client.reply(message, "no upcoming raids");
+        }
+
+        // Alert each raid to channel
+        var msg = "**Upcoming raids**\n"
+        raids.forEach(function(raid, idx) {
+            msg += ` - #${idx} ~ ${raid['desc']} on **${moment(raid['time']).format("ddd Do "/*MMM*/+" @ HH:mm")}**\n`  
+        })
+        client.reply(message, msg);
+    }
+
+    if(message.content.indexOf("!killraid ") === 0) {
+        if (!Object.keys(raids).length) {
+            return client.reply(message, "no upcoming raids");
+        }
+
+        var raidId = betterSplit(message.content)[1]
+
+        if (!raids.hasOwnProperty(raidId)) {
+            return client.reply(message, "invalid raid");
+        }
+
+        delete raids[raidId]
+
+        client.reply(message, "raid deleted");
     }
 
     if(message.content.indexOf("!invasion") === 0) {
@@ -57,7 +146,7 @@ client.on("message", function(message) {
         var text_hour = ((hour_left != 1) ? "hours" : "hour"),
         text_minute = ((minute_left != 1) ? "minutes" : "minute");
 
-        return  client.reply(message, `next Invasion starts in ${hour_left} ${text_hour} ${minute_left} ${text_minute}`);
+        return client.reply(message, `next Invasion starts in ${hour_left} ${text_hour} ${minute_left} ${text_minute}`);
     }
 });
 
